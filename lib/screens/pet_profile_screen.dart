ed42c08d01ba52cb409e6a1c105d48e4ee1b678a
ptthvy <24100439@st.phenikaa-uni.edu.vn>
2026-08-03 14:07:06 +0000

    Khởi tạo màn hình hồ sơ thú cưng

---
+++ lib/screens/pet_profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PetProfileScreen extends StatefulWidget {
+  const PetProfileScreen({super.key});
+
+  @override
+  State<PetProfileScreen> createState() => _PetProfileScreenState();
+}
+
+class _PetProfileScreenState extends State<PetProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text('Hồ sơ thú cưng'),
+      ),
+    );
+  }
+}

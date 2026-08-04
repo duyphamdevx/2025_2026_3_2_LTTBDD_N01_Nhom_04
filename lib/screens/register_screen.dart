26fa7aba0c22be468744e0b9a061522ef3a19361
NguyenHoangNam47 <24100136@st.phenikaa-uni.edu.vn>
2026-08-04 13:12:15 +0000

    khởi tạo màn hình đăng ký

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -3,3 +3,10 @@ import '../theme/app_theme.dart';
 import '../state/app_state.dart';
 import '../l10n/app_strings.dart';
 import '../widgets/language_badge.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}

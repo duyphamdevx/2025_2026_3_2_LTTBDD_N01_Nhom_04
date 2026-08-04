1bd628a8b5f3044cbaddc5be8e782ace04643a0c
NguyenHoangNam47 <24100136@st.phenikaa-uni.edu.vn>
2026-08-04 12:52:59 +0000

    liên kết các widget và màn hình

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 import '../state/app_state.dart';
 import '../l10n/app_strings.dart';
+import '../widgets/bottom_nav.dart';
+import '../widgets/language_badge.dart';
+import 'register_screen.dart';
+import 'forgot_password_screen.dart';

eab2ea5d0fa9a3784984986a5e004180d183c236
NguyenHoangNam47 <24100136@st.phenikaa-uni.edu.vn>
2026-08-04 12:56:29 +0000

    khởi tạo ẩn mật khẩu

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,3 +17,4 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _obscurePassword = true;

e210d5ed85862e576e692db4c39bd26d8426b854
NguyenHoangNam47 <24100136@st.phenikaa-uni.edu.vn>
2026-08-04 13:11:42 +0000

    widget chuyển đổi ngôn ngữ

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 import '../state/app_state.dart';
 import '../l10n/app_strings.dart';
+import '../widgets/language_badge.dart';
